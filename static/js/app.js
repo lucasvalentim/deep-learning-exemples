6685364ebe58de7e67aab92cbd7435b19878673e
Lucas Valentim <lucasvalentimdias@gmail.com>
2018-12-19 23:20:45 -0300

    Inserção do preview das imagens

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,42 @@
 $(function() {
-    $("#mnist form").on('submit', function(event) {
-        event.preventDefault();
-        
-        var formData = new FormData(this);
+    $('#mnist .input-image').on('change', function(event) {
+        if(this.files && this.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#mnist .image-preview').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(this.files[0]);
+
+            $('.image-preview-wrapper').show();
+
+            var formData = new FormData();
+
+            formData.append('image', this.files[0]);
+
+            $.ajax({
+                url: '/api/mnist/predict',
+                type: 'POST',
+                data: formData,
+                success: function(data) {
+                    console.log(data)
+                },
+                cache: false,
+                contentType: false,
+                processData: false,
+                xhr: function() { // Custom XMLHttpRequest
+                    var myXhr = $.ajaxSettings.xhr();
     
-        $.ajax({
-            url: '/api/mnist/predict',
-            type: 'POST',
-            data: formData,
-            success: function(data) {
-                console.log(data)
-            },
-            cache: false,
-            contentType: false,
-            processData: false,
-            xhr: function() { // Custom XMLHttpRequest
-                var myXhr = $.ajaxSettings.xhr();
-
-                if(myXhr.upload) { // Avalia se tem suporte a propriedade upload
-                    myXhr.upload.addEventListener('progress', function() {
-                        /* faz alguma coisa durante o progresso do upload */
-                    }, false);
+                    if(myXhr.upload) { // Avalia se tem suporte a propriedade upload
+                        myXhr.upload.addEventListener('progress', function() {
+                            // faz alguma coisa durante o progresso do upload
+                        }, false);
+                    }
+    
+                    return myXhr;
                 }
-
-                return myXhr;
-            }
-        });
+            });
+        }
     });
 });
